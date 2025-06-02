b5e109f4403f5c8f9b547b9931bfec536daffc39
Tajuddin-green <cmtajuddinchowdhury@gmail.com>
2025-06-03 02:34:26 +0600

    added get tips by id and get details by id

--- index.js
+++ index.js
@@ -1,57 +1,77 @@
 const express = require("express");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
 const cors = require("cors");
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdkra1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  tls: true,
+  serverSelectionTimeoutMS: 3000,
+  autoSelectFamily: false,
 });
-
 async function run() {
   try {
     await client.connect();
-    const plantCollection = client.db("plantDB").collection("plants");
 
-app.post('/plants', async(req, res)=>{
+    const db = client.db("plantBook");
+    const gardenersCollection = db.collection("featuredGardeners");
+    const tipsCollection = db.collection("allTips");
 
-})
+    app.get("/gardeners", async (req, res) => {
+      const activeGardeners = await gardenersCollection
+        .find({ active: true })
+        .limit(6)
+        .toArray();
+      res.send(activeGardeners);
+    });
 
+    app.get("/gardeners/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gardenersCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    app.get("/plantdetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tipsCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    app.post("/addnewtip", async (req, res) => {
+      const newTip = req.body;
+      const result = await tipsCollection.insertOne(newTip);
+      res.send(result);
+    });
 
+    app.get("/alltips", async (req, res) => {
+      const query = { availability: "Public" };
+      const result = await tipsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-    // You can define MongoDB-dependent routes here
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
   }
 }
 run().catch(console.dir);
 
-// Define general routes
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Listening on port :${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
